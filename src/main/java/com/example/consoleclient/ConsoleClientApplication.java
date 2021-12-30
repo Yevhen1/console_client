eee3fd92673308adaccfcc14b313fdd2c60c9a30
Yevhen <viatoris8@gmail.com>
2021-12-30 17:18:26 +0200

    add log.txt

--- src/main/java/com/example/consoleclient/ConsoleClientApplication.java
+++ src/main/java/com/example/consoleclient/ConsoleClientApplication.java
@@ -3,18 +3,25 @@ package com.example.consoleclient;
 import com.example.consoleclient.model.Book;
 import com.example.consoleclient.model.Bookmark;
 import com.example.consoleclient.model.Reader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
@@ -26,15 +33,26 @@ import java.util.List;
 @SpringBootApplication
 public class ConsoleClientApplication {
 
-	Logger logger = LoggerFactory.getLogger(ConsoleClientApplication.class);
+	Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
+//		System.setProperty("org.springframework.boot.logging.LoggingSystem",
+//				"org.springframework.boot.logging.log4j2.Log4J2LoggingSystem");
 		SpringApplication.run(ConsoleClientApplication.class, args);
 	}
 
 	private final String URL = "http://localhost:8080/";
 	private String readerName;
 	private int id;
+
+//	@Bean
+//	public HttpMessageConverters customConverters(ApplicationContext context) {
+//		Jackson2ObjectMapperBuilder builder = Jackson2ObjectMapperBuilder.json();
+//		if (context != null) {
+//			builder.applicationContext(context);
+//		}
+//		return new HttpMessageConverters(new MappingJackson2HttpMessageConverter(builder.build()));
+//	}
 
 	@Bean
 	public CommandLineRunner run(){
@@ -49,7 +67,7 @@ public class ConsoleClientApplication {
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			result = reader.readLine();
-			logger.info(result);
+			System.out.println(result);
 		}catch (IOException io){
 			logger.error(io.getMessage());
 		}
@@ -58,17 +76,17 @@ public class ConsoleClientApplication {
 
 
 	private void login(){
-		logger.info("Enter you name:");
+		System.out.println("Enter you name:");
 		postReader(readLine());
 	}
 
 
 	private void writeDescription(){
-		logger.info("====================================");
-		logger.info("addBook");
-		logger.info("setBookMark");
-		logger.info("getBooks");
-		logger.info("====================================");
+		System.out.println("====================================");
+		System.out.println("addBook");
+		System.out.println("setBookMark");
+		System.out.println("getBooks");
+		System.out.println("====================================");
 
 	}
 
@@ -76,7 +94,7 @@ public class ConsoleClientApplication {
 		writeDescription();
 
 		for (;;){
-			logger.info("Enter you command: ");
+			System.out.println("Enter you command: ");
 			String []command = readLine().split(" ");
 
 			switch (command[0]){
@@ -101,12 +119,12 @@ public class ConsoleClientApplication {
 		ResponseEntity<Reader> response = restTemplate.exchange(
 				URL + "reader/", HttpMethod.POST, request, Reader.class);
 		int statusCode = response.getStatusCode().value();
-		logger.info(String.valueOf(statusCode));
+		System.out.println(String.valueOf(statusCode));
 		if (statusCode == 201 || statusCode == 200){
 			id = response.getBody().getId();
 			readerName = response.getBody().getReaderName();
-			logger.info("You id is: " + id);
-			logger.info("You name is: " + readerName);
+			System.out.println("You id is: " + id);
+			System.out.println("You name is: " + readerName);
 		}
 	}
 
@@ -116,21 +134,21 @@ public class ConsoleClientApplication {
 		HttpEntity<Book> request = new HttpEntity<>(new Book(bookName, Integer.valueOf(count)));
 		ResponseEntity<HttpStatus> response = restTemplate.exchange(
 				URL + "addBook/" + id, HttpMethod.POST, request, HttpStatus.class);
-		logger.info(String.valueOf(response.getStatusCode().value()));
+		System.out.println(String.valueOf(response.getStatusCode().value()));
 	}
 
 	private void setBook(String bookName, String pageNumber){
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<HttpStatus> response = restTemplate.getForEntity(
 				URL + "setBookMark/" + id + "/" + bookName + "/" + pageNumber, HttpStatus.class);
-		logger.info(String.valueOf(response.getStatusCode().value()));
+		System.out.println(String.valueOf(response.getStatusCode().value()));
 	}
 
 	private void getBooks(){
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(
 				URL + "getAllBooks/" + id, String.class);
-		logger.info(String.valueOf(response.getStatusCode().value()));
-		logger.info(response.getBody());
+		System.out.println(String.valueOf(response.getStatusCode().value()));
+		System.out.println(response.getBody());
 	}
 }
